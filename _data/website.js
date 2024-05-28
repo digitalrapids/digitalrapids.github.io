e7302d061c08e51a13fcab6cfbac7882f796c793
jraedisch <jasper@raedisch.net>
2024-05-28 09:16:52 +0200

    import website

---
+++ _data/website.js
@@ -0,0 +1,200 @@
+export default {
+  menu: [
+    {
+      de: 'Home',
+      en: 'Home'
+    },
+    {
+      de: 'Kontakt',
+      en: 'Contact us'
+    },
+    {
+      de: 'Unternehmen',
+      en: 'Company'
+    },
+    {
+      de: 'Leistungen',
+      en: 'Services'
+    },
+    {
+      de: 'Arbeiten',
+      en: 'Projects'
+    },
+    {
+      de: 'Blog',
+      en: 'Blog'
+    }
+  ],
+  blocks: {
+    intro: {
+      overline: {
+        de: 'Interim-Management auf Führungsebene',
+        en: 'TBD'
+      },
+      headline: {
+        de: 'Digital zukünftsfähig sein.',
+        en: 'TBD'
+      },
+      cta: {
+        de: 'Kennenlerntermin',
+        en: 'TBD'
+      }
+    },
+    company: {
+      overline: {
+        de: 'Unser Unternehmen',
+        en: 'TBD'
+      },
+      headline_minor: {
+        de: 'Ihr Erfolg ist unser Antrieb.',
+        en: 'TBD'
+      },
+      description: {
+        de: `Fehlendes Personal auf C- oder Top-Level, digitaler Umbau, Scale-Up oder Umstrukturierung - digital rapids ist Ihr Partner für Interim-Management auf Führungsebene. 
+
+        Digitalisierung ist Teil unserer DNA. Diese Expertise übertragen wir auf unsere Kunden. Wir führen Sie operativ und strategisch klug durch digitale Veränderungsprozesse, um die Weichen auf Erfolg zu stellen.`,
+        en: 'TBD'
+      },
+      cta_minor: {
+        de: 'Mehr erfahren',
+        en: 'TBD'
+      },
+      image: '/assets/foto_platzhalter_anne.jpg'
+    },
+    services: {
+      overline: {
+        de: 'Unsere Leistungen',
+        en: 'TBD'
+      }
+    },
+    team: {
+      overline: {
+        de: 'Unser Team',
+        en: 'TBD'
+      },
+      headline_minor: {
+        de: 'Wir bringen Sie ans Ziel!',
+        en: 'TBD'
+      },
+      description: {
+        de: `Wir sind Leader, Umsetzer, Enabler, Strategen, Macher, Antreiber, Experten und Partner. Und oft auch Feuerlöscher. Beste Voraussetzungen, um effizient genau dort einzugreifen, wo Sie uns brauchen. 
+
+        Wir übernehmen zeitnah Führungsaufgaben in Ihrem Unternehmen. Als erfahrene Interim-Manager für die digitale Transformation helfen wir Ihnen, digitale Prozesse und Geschäftsmodelle durch das Vernetzen von digitaler Technik, Informationen und den Menschen in Ihrem Unternehmen erfolgreich umzusetzen. 
+        
+        Unser Versprechen dabei: Wir sind immer an Ihrer Seite – vom ersten Tag, bis wir gemeinsam das Ziel erreicht haben.`,
+        en: 'TBD'
+      },
+      image: '/assets/foto_platzhalter_anne.jpg'
+    }
+  },
+  services: [
+    {
+      name: {
+        de: 'Digital Strategy',
+        en: 'TBD'
+      },
+      description: {
+        de: 'Gemeinsame, agile Entwicklung Ihrer Digitalisierungsstrategie',
+        en: 'TBD'
+      }
+    },
+    {
+      name: {
+        de: 'C- & Top-Level Interim Management',
+        en: 'TBD'
+      },
+      description: {
+        de: 'Interim Manager auf Führungsebene übernehmen Verantwortung und Operations',
+        en: 'TBD'
+      }
+    },
+    {
+      name: {
+        de: 'Digital Change',
+        en: 'TBD'
+      },
+      description: {
+        de: 'Aktive Begleitung und Gestaltung Ihrer Transformationsprozesse',
+        en: 'TBD'
+      }
+    },
+    {
+      name: {
+        de: 'Executive & Board Advisory',
+        en: 'TBD'
+      },
+      description: {
+        de: 'Langfristige Begleitung bei digitaler Transformation & Corporate Governance',
+        en: 'TBD'
+      }
+    },
+    {
+      name: {
+        de: 'M&A Advisory',
+        en: 'TBD'
+      },
+      description: {
+        de: 'Unterstützung bei der Datenraumarbeit & Gutachten zu Technik & Geschäftsmodellen',
+        en: 'TBD'
+      }
+    },
+    {
+      name: {
+        de: 'Digital Operations',
+        en: 'TBD'
+      },
+      description: {
+        de: 'Vereinfachung Ihrer Prozesse, Erarbeitung von Kosteneffizienzen, Datenmanagement',
+        en: 'TBD'
+      }
+    }
+  ],
+  team: {
+    focus: {
+      de: 'Schwerpunkte',
+      en: 'TBD'
+    },
+    customers: {
+      de: 'Kunden',
+      en: 'TBD'
+    },
+    managers: [
+      {
+        fullName: 'Felix Menden',
+        title: 'CEO',
+        focus: {
+          de: 'CEO, CIO, CTO, CPO, CDO, General Management, IT, Software, Digitale Produkte, Transformation, M&A / Due Diligence, Post-Merger Integration',
+          en: 'TBD'
+        },
+        customers: {
+          de: 'u.a. Deutscher Mittelstand, div. Europäischer Einzelhandel / eCommerce, Energieversorger, Staatsunternehmen, Börsennotierte Internetunternehmen, Europäische Verlage, div. Banken',
+          en: 'TBD'
+        }
+      },
+      {
+        fullName: 'Anne Schönbrun',
+        title: 'COO',
+        focus: {
+          de: 'CMO, CPO, General Management, Project Management, Product Management, Post Merger Integration, Transformation Management, Due Diligence, Agile Strategieentwicklung',
+          en: 'TBD'
+        },
+        customers: {
+          de: 'Energieversorger, Marketplace B2B, Staatl. Glückspiel, Verlagswesen',
+          en: 'TBD'
+        }
+      },
+      {
+        fullName: 'Daniel Brückmann',
+        title: 'Principal',
+        focus: {
+          de: 'CPO, Product Management, Organisationsaufbau Product, Digitalisierungsprojekte, Transformation Management',
+          en: 'TBD'
+        },
+        customers: {
+          de: 'Energieversorger, Mobility, Banking, Tech Branche',
+          en: 'TBD'
+        }
+      }
+    ]
+  }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./_components/**/*.webc', './_includes/**/*.webc'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        waves: "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/assets/wellen_hintergrund.svg')"
+      },
+      colors: {
+        drbeige: '#fcfbfa',
+        drdarkgray: '#333333',
+        drgold: '#ceb37e',
+        drlightgray: '#c6c6c6',
+        drpink: '#fa2969'
+      },
+      flexGrow: {
+        2: '2'
+      },
+      fontFamily: {
+        sans: ['Noto Sans']
+      },
+      fontSize: {
+        base: '0.875rem'
+      }
+    }
+  },
+  plugins: []
+}
