c1841d598822262978563e9bc4cfe66d36dcc317
jraedisch <jasper@raedisch.net>
2024-05-28 10:57:17 +0200

    add image converter

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,16 +1,34 @@
 import { EleventyI18nPlugin } from '@11ty/eleventy'
 import pluginWebc from '@11ty/eleventy-plugin-webc'
+import { eleventyImagePlugin } from "@11ty/eleventy-img";
 
 export default function (eleventyConfig) {
   eleventyConfig.addPlugin(EleventyI18nPlugin, {
     defaultLanguage: 'de'
   })
-  eleventyConfig.addPlugin(pluginWebc)
+
   eleventyConfig.addJavaScriptFunction("dr18", (obj) => obj[page.lang] || obj);
   eleventyConfig.addJavaScriptFunction("drFA", (name) => `#fa-${name}`);
   eleventyConfig.addJavaScriptFunction("drExists", (thing) => !!thing);
+
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("robots.txt");
   eleventyConfig.addPassthroughCopy("favicon.ico");
+
+  eleventyConfig.addPlugin(pluginWebc, {
+		components: [
+      "_components/**/*.webc",
+			"npm:@11ty/eleventy-img/*.webc",
+		],
+	});
+
+  eleventyConfig.addPlugin(eleventyImagePlugin, {
+    formats: ["webp", "jpeg"],
+    urlPath: "/img/",
+    defaultAttributes: {
+      loading: "lazy",
+      decoding: "async",
+    },
+  });
 }
